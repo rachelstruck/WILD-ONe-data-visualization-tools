d0c9f9ccb0821d0fa4fb2edf3f0a66041848c52a
SimpleMilk <strucktanner@gmail.com>
2020-05-05 02:35:41 -0500

    Added many more features to toolbox. Includes variable selector window and a window for selected variables to appear. A complete variable selection tool is still a work in progress.

--- root.py
+++ root.py
@@ -1,22 +1,23 @@
 # Main window of the Application
 
-import tkinter as tk
+from tkinter import *
 from toolbox import ConditionSelect
 
 # Creates main window
-root = tk.Tk()
+root = Tk()
 root.title('Hello World')
 
 # Create button panel at the left of the window
-ButtonFrame = tk.Frame(root)
-tk.Button(ButtonFrame, text='Home', width=10).pack()
-tk.Button(ButtonFrame, text='Spreadsheet', width=10).pack()
-tk.Button(ButtonFrame, text='Time Plot', width=10).pack()
+ButtonFrame = Frame(root)
+Button(ButtonFrame, text='Home', width=10).pack()
+Button(ButtonFrame, text='Spreadsheet', width=10).pack()
+Button(ButtonFrame, text='Time Plot', width=10).pack()
+Button(ButtonFrame, text='Pie Chart', width=10).pack()
 ButtonFrame.grid(column=0, row=0)
 
 # Creates content at the right
-ContentFrame = tk.Frame(root)
-tk.Label(ContentFrame, text='here is some text you can see').pack()
+ContentFrame = Frame(root)
+Label(ContentFrame, text='here is some text you can see').pack()
 ContentFrame.grid(column=1, row=0)
 
 root.mainloop()
--- toolbox.py
+++ toolbox.py
@@ -1,6 +1,12 @@
 # A set of tools to more easily build frames.
 
 from tkinter import *
+import pandas as pd
+
+print("loading case data...")
+datapath = r"C:\Users\buffs\Documents\cases.xls"
+cases = pd.read_html(datapath)[0]
+print("done\n")
 
 ConditionDict = {}
 
@@ -9,7 +15,8 @@ class Condition:
     def __init__(self, name, isgrouped=False):
         self.name = name
         self.isgrouped = isgrouped
-        #self.series = cases[self.name]
+        self.series = cases[self.name].drop_duplicates()
+        self.array = self.series.dropna().array
 
         ConditionDict[self.name] = self
 
@@ -23,11 +30,9 @@ Disposition_Addit = Condition('Disposition Addit.')
 Disposition_Jurisdiction = Condition('Disposition Jurisdiction')
 To_Whom = Condition('To Whom')
 
-class ConditionSelect:
+defaultCondition = list( ConditionDict.keys() )[0]
 
-    def UpdateDisplay(self, name):
-        return lambda: self.set(name)
-
+class TkWidget:
     def grid(self, **kwargs):
         self.object.grid(**kwargs)
         return self
@@ -36,15 +41,27 @@ class ConditionSelect:
         self.object.pack(**kwargs)
         return self
 
+class ConditionSelect(TkWidget):
+
+    def UpdateDisplay(self, name):
+        self.set(name)
+        self.tracefunc()
+    def UpdateDisplayCommand(self, name):
+        return lambda: self.UpdateDisplay(name)
+
+    def getCondition(self):
+        name = self.get()
+        return ConditionDict[name]
+
+    def setTracefunc(self, tracefunc):
+        self.tracefunc = tracefunc
+
     def __init__(self, frame):
         self.stringvar = StringVar()
         self.get = self.stringvar.get
         self.set = self.stringvar.set
-        self.set(list(ConditionDict.keys())[0])
-
-        def getCondition(self):
-            name = self.get()
-            return ConditionDict[name]
+        self.set(defaultCondition)
+        self.tracefunc = lambda: None
 
         self.object = Menubutton(
             frame, textvariable=self.stringvar, relief='raised')
@@ -52,7 +69,64 @@ class ConditionSelect:
         self.object['menu'] = self.menu
 
         for name in ConditionDict.keys():
-            self.menu.add_command(label=name, command=self.UpdateDisplay(name))
+            self.menu.add_command(label=name,
+                command=self.UpdateDisplayCommand(name))
+
+class VarWindow(TkWidget):
+
+    def buildBox(self, condition):
+        self.condition = condition
+        varlist = self.condition.array
+        self.object.delete(0, 'end')
+        for i in range(len(varlist)):
+            self.object.insert(i, varlist[i])
+
+    def filter(self, string):
+        varlist = self.condition.array
+        self.object.delete(0, 'end')
+        for var in varlist:
+            if string in var:
+                self.object.insert(self.object.size(), var)
+
+    def getSelected(self):
+        index = self.object.curselection()
+        selected = []
+        for i in index:
+            selected.append(self.object.get(i))
+        return selected
+
+    def __init__(self, frame):
+        self.sbar = Scrollbar(frame)
+        self.sbar.pack(side='right', fill='y')
+        self.object = Listbox(frame, yscrollcommand=self.sbar.set,
+            width=50, height=19, selectmode='extended')
+        self.sbar.config(command=self.object.yview)
+
+        self.buildBox( ConditionDict[defaultCondition] )
+
+class SelectedWindow(TkWidget):
+
+    def add(self, varlist):
+        for var in varlist:
+            self.object.insert(self.object.size(), var)
+
+    def remove(self):
+        index = list( self.object.curselection() )
+        index.sort(reverse=True)
+        for i in index:
+            self.object.delete(i)
+
+    def __init__(self, frame):
+        self.object = Listbox(frame, width=50, height=19,
+            selectmode='extended')
+
+class VarSelector(TkWidget):
+
+    def __init__(self, frame):
+        self.frame = Frame(frame)
+        self.ConditionSelector = ConditionSelect().grid(row=0, column=0)
+        self.VarWindow = VarWindow().grid(row=1, column=0)
+        self.SelectedWindow = SelectedWindow().grid(row=1, column=1)
 
 if __name__ == '__main__':
 
@@ -60,7 +134,26 @@ if __name__ == '__main__':
 
     root = Tk()
 
+    box = VarWindow(root)
+
+
     b = ConditionSelect(root)
+    def f():
+        box.buildBox(b.getCondition())
+    b.setTracefunc(lambda: box.buildBox(b.getCondition()))
     b.pack()
+
+    p = Button(
+        root, text="print selection",
+        command=lambda: selected.add( box.getSelected() )
+        )
+    p.pack()
+
+    box.pack()
+
+    selected = SelectedWindow(root).pack()
+    delButton = Button(
+        root, text="delete", command=lambda: selected.remove()
+        ).pack()
 
     root.mainloop()
