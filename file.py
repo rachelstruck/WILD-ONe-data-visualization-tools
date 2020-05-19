c616715a4448113fb29a73061bf1421b87561f1b
Tanner Struck <strucktanner@gmail.com>
2020-05-19 00:29:20 -0500

    Fixed the bug with the "GO" button used for testing and left a block comment explaining the bug.

--- file.py
+++ file.py
@@ -10,7 +10,15 @@ class File(Frame):
         self.label.pack()
         self.entry = Entry(self)
         self.entry.pack()
-        self.button = Button(text="GO", width=10, command=load_data(self.entry.get()))
+        self.button = Button(text="GO", width=10,
+                             command=lambda: load_data(self.entry.get()))
+        # Here, we have to use a lambda function because the command argument
+        # only accepts function type arguments. If you call the function
+        # you are trying to set command to, then you are setting command
+        # to the output of the function, not the function itself. Using
+        # lambda, we can create a function that, when called, does what
+        # you want the button to do, which is exactly what we want command
+        # to be.
         self.button.pack()
 
 if __name__ == "__main__":
